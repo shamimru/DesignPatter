988bd61fae0f66d568193cea7ba898cbdb4b6360
shamimAhamed <shammeedd@gmail.com>
2024-10-25 22:56:09 +0600

    new Design patter programme has been started

---
+++ src/main/java/com/spring/designPattern/DesignPatterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.designPattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/designPattern/DesignPatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.designPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
